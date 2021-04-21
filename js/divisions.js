6cf5f401b0d7b9ffd19bfc7fc8f0460b70986b03
diptish.ghosh <diptish.ghosh@niit.com>
2021-04-21 12:00:27 +0530

    jquery hands on with division

---
+++ js/divisions.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+$('#button1').click(function(){
+    $("div[name$='DAIntelligence']").css( "background", "yellow" );
+    $("div[name$='ASI_Intelligence']").css( "background", "yellow" );
+});
+});
